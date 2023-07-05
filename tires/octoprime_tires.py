9ec397f530df9e4ba7db3348cd54ed308b187118
0xPorkchops <proulxcameron@icloud.com>
2023-07-04 21:01:25 -0400

    Changed Octoprime tires service threshold from int to float

--- tires/octoprime_tires.py
+++ tires/octoprime_tires.py
@@ -5,4 +5,4 @@ class OctoprimeTires(Tires):
         self.tire_wear_readings = tire_wear_readings
 
     def needs_service(self):
-        return sum(self.tire_wear_readings) >= 3
+        return sum(self.tire_wear_readings) >= 3.0
